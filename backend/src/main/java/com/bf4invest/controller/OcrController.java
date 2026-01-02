b0382d917904c8ee1336bf101e0f61e486526a15
BOUNORAli <bounor.ali@example.com>
2026-01-02 12:12:37 +0100

    add ocr

--- backend/src/main/java/com/bf4invest/controller/OcrController.java
+++ backend/src/main/java/com/bf4invest/controller/OcrController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/ocr")
+@RequestMapping("/ocr")
 @RequiredArgsConstructor
 @Slf4j
 public class OcrController {
